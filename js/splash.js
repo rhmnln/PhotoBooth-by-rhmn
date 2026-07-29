d4147f92ec088fb0285ffd8b53021c806fa8d718
Rahman Maulana <rhmnln11@gmail.com>
2026-07-29 16:35:58 +0800

    Create splash.js

---
+++ js/splash.js
@@ -0,0 +1,18 @@
+(function () {
+  const splash = document.getElementById('splashScreen');
+  if (!splash) return;
+
+  if (sessionStorage.getItem('pb_splash_shown')) {
+    splash.classList.add('is-hidden');
+    return;
+  }
+
+  function dismiss() {
+    sessionStorage.setItem('pb_splash_shown', '1');
+    splash.classList.add('is-leaving');
+    setTimeout(() => splash.classList.add('is-hidden'), 650);
+  }
+
+  splash.addEventListener('click', dismiss);
+  setTimeout(dismiss, 2200);
+})();
